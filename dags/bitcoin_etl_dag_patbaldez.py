7bb14e0767f578ed0782b05bbd89e60e3f7a8308
PatBaldez <patbaldez@gmail.com>
2025-09-25 22:56:51 -0300

    Create bitcoin_etl_dag_patbaldez.py

---
+++ dags/bitcoin_etl_dag_patbaldez.py
@@ -0,0 +1,11 @@
+{
+  "type": "service_account",
+  "project_id": "patbigcoin",
+  "client_email": "airflow-writer@patbigcoin.iam.gserviceaccount.com",
+  "client_id": "108390027228078124109",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/airflow-writer%40patbigcoin.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
